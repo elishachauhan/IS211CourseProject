055d68832a9808c8343f1937dd135c89914e218c
Elisha Chauhan <elisha.chauhan@spsmail.cuny.edu>
2016-12-18 20:40:58 +0000

    Updated schema file

--- load_schema.py
+++ load_schema.py
@@ -6,7 +6,7 @@ import sqlite3
 
 
 def main():
-    conn = sqlite3.connect('myblog.db')
+    conn = sqlite3.connect('musicblog.db')
     f = open('schema.sql','r')
     sql = f.read()
     conn.executescript(sql)
